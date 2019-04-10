e8855d255a62e4e50f71e0b8be54d24e59a97cfa
OldIronEyes <VKChalla045@gmail.com>
2019-04-09 21:14:41 -0400

    Client Function prototypes added

---
+++ WTF.h
@@ -0,0 +1,15 @@
+#ifndef WTF_H
+#define WTF_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+ * These are the default IP Address/Port that will be used. If a .Manifest file exists
+ * then they will be overwritten as necessary.
+ */
+ 
+//char* IPAddress = ;
+//char* portNumber = ;
+
+#endif
--- WTFClient.c
+++ WTFClient.c
@@ -1,4 +1,81 @@
-#include "WTFClient.h"
+#include "WTF.h"
+
+
+/*
+ * Client should attempt to connect every three 
+ * seconds until successful or interrupted by SIGINT
+ * returns 1 when a connection is established
+ */
+
+int connect(char* IPAddress, int portNumber){
+
+}
+
+/*
+ * Should write/update the .Manifest file to update the
+ * connection variables as necessary, and update the global
+ * variables defined in WTF.h
+ */
+
+void configure(char* newIP, int newPort){
+
+}
+
+void checkout(char* projectName){
+
+}
+
+void update(char* projectName){
+
+}
+
+void upgrade(char* projectName){
+
+}
+
+void commit(char* projectName){
+
+}
+
+void push(char* projectName){
+
+}
+
+void create(char* projectName){
+
+}
+
+void destroy(char* projectName){
+
+}
+
+void add(char* projectName, char* fileName){
+
+}
+
+void remove(char* projectName, char* fileName){
+
+}
+
+void currentVersion(char* projectName){
+
+}
+
+void history(char* projectName){
+
+}
+
+void rollBack(char* projectName, int versionNumber){
+
+}
+
+
+/*
+ * This code looks disgusting but there's no way to use strings in
+ * switch statements in C, so this is the next best option. Each of the
+ * printf statements should eventually be replaced with the appropriate
+ * function call.
+ */
 
 int main(int argc, char *argv[]){
 	if(argv[1] == "checkout"){
