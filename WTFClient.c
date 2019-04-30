e608400bd29862c83e03af1c03fdee59c7516dac
OldIronEyes <VKChalla045@gmail.com>
2019-04-30 14:44:35 -0400

    Wrote clientside code

--- WTFClient.c
+++ WTFClient.c
@@ -1,137 +1,75 @@
 #include "WTF.h"
 
-/*
- * Client should attempt to connect every three 
- * seconds until successful or interrupted by SIGINT
- * returns 1 when a connection is established
- */
-
-int connect(char* IPAddress, int portNumber){
-
-}
-
-/*
- * Should write/update the .config file to update the
- * connection variables as necessary, and update the global
- * variables defined in WTF.h
- */
-
-void configure(char* newIP, int newPort){
-
-}
-
-void writeManifest(){
-
-}
-
-void checkout(char* projectName){
-
-}
-
-void update(char* projectName){
-
-}
-
-void upgrade(char* projectName){
-
-}
-
-void commit(char* projectName){
-
-}
-
-void push(char* projectName){
-
-}
-
-void create(char* projectName){
-
-}
-
-void destroy(char* projectName){
-
-}
-
-void add(char* projectName, char* fileName){
-
-}
-
-void remove(char* projectName, char* fileName){
-
-}
-
-void currentVersion(char* projectName){
-
-}
-
-void history(char* projectName){
-
-}
-
-void rollBack(char* projectName, int versionNumber){
-
-}
-
-
-/*
- * This code looks disgusting but there's no way to use strings in
- * switch statements in C, so this is the next best option. Each of the
- * printf statements should eventually be replaced with the appropriate
- * function call.
- */
-
-int main(int argc, char *argv[]){
-	if(argv[1] == "checkout"){
-	
-		printf("checkout\n");
-	
-	} else if (argv[1] == "update"){
-	
-		printf("update\n");
-	
-	} else if (argv[1] == "upgrade"){
-	
-		printf("upgrade\n");
-	
-	} else if (argv[1] == "commit"){
-	
-		printf("commit\n");
-	
-	} else if (argv[1] == "push"){
-	
-		printf("push\n");
-	
-	} else if (argv[1] == "create"){
-	
-		printf("create\n");
-	
-	} else if (argv[1] == "destroy"){
-	
-		printf("destroy\n");
-	
-	} else if (argv[1] == "add"){
-	
-		printf("add\n");
-	
-	} else if (argv[1] == "remove"){
-	
-		printf("remove\n");
-	
-	} else if (argv[1] == "currentversion"){
-	
-		printf("currentversion\n");
-	
-	} else if (argv[1] == "history"){
-	
-		printf("history\n");
-	
-	} else if (argv[1] == "rollback"){
-	
-		printf("rollback\n");
-	
-	} else {
-	
-		printf("Invalid Command\n");
+int main(int argc, char* argv[]){
+	if(strcmp(argv[1], "configure") == 0){
+		printf("configure");
 	}
-	
+
+	else if (strcmp(argv[1],"create") = 0){
+		if(argc != 2){
+			printf("Incorrect number of arguments");
+		} else {
+			createProject(argv[2]);
+		}
+	}
+
+	else if (strcmp(argv[1],"destroy") = 0){
+		if(argc != 2){
+			printf("Incorrect number of arguments");
+		} else {
+			createProject(argv[2]);
+		}
+	}
+
+	else if (strcmp(argv[1],"add") = 0){
+		if(argc != 3){
+			printf("Incorrect number of arguments");
+		} else {
+			addFile(argv[2], argv[3]);
+		}
+	}
+
+	else if (strcmp(argv[1],"remove") = 0){
+		if(argc != 3){
+			printf("Incorrect number of arguments");
+		} else {
+			removeFile(argv[2], argv[3]);
+		}
+	}
+
+	else if (strcmp(argv[1],"checkout") = 0){
+		printf("checkout");
+	}
+
+	else if (strcmp(argv[1],"update") = 0){
+		printf("update");
+	}
+
+	else if (strcmp(argv[1],"upgrade") = 0){
+		printf("upgrade");
+	}
+
+	else if (strcmp(argv[1],"commit") = 0){
+		printf("commit");
+	}
+
+	else if (strcmp(argv[1],"push") = 0){
+		printf("push");
+	}
+
+	else if (strcmp(argv[1],"currentversion") = 0){
+		printf("current version");
+	}
+
+	else if (strcmp(argv[1],"history") = 0){
+		printf("history");
+	}
+
+	else if (strcmp(argv[1],"rollback") = 0){
+		printf("rollback");
+	}
+
+	else {
+		printf("Invalid Comand");
+	}
 }
