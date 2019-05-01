fdacc0d318463265b3481ce6498ab39d476b0fbf
Dovk <33917053+SMagnet22@users.noreply.github.com>
2019-05-01 10:03:39 -0400

    The working server without the port check

--- WTFserver.c
+++ WTFserver.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h> 
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 
@@ -19,12 +19,12 @@ void* func(void *vargp)
     printf("new connection\n");
     while (1)
     {
-         if (read(*fd, buffer, 255) < 0)
-         {
-             perror("read");
-             close(*fd);
-             return NULL;
-         }
+        if (read(*fd, buffer, 255) < 0)
+        {
+            perror("read");
+            close(*fd);
+            return NULL;
+        }
         printf("receive message:%s\n", buffer);
         return 0;
         if (strcmp(buffer, "done") == 0)
@@ -36,45 +36,37 @@ void* func(void *vargp)
 
 int main(int argc, char *argv[])
 {
-     int sockfd, newsockfd, portno;
-     socklen_t clilen_addr_size;
-     struct sockaddr_in serv_addr, cli_addr;
-     pthread_t thread;
-     if (argc < 2) {
-         fprintf(stderr,"ERROR, no port provided\n");
-         exit(1);
-     }
-     
-     int valid_port = atoi(argv[1]);
-
-     if (valid_port> 64000 || valid_port<8000){
-         printf("Please enter a port number between 8K and 64K\n");
-         return 0;
-     }
-
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0) 
+    int sockfd, newsockfd, portno;
+    socklen_t clilen_addr_size;
+    struct sockaddr_in serv_addr, cli_addr;
+    pthread_t thread;
+    if (argc < 2) {
+        fprintf(stderr,"ERROR, no port provided\n");
+        exit(1);
+    }
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
         error("ERROR opening socket");
-     portno = atoi(argv[1]);
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
-     if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0) 
-              error("ERROR on binding");
-     listen(sockfd,5);
-     clilen_addr_size = sizeof(cli_addr);
-     while (1)
-     {
-         newsockfd = accept(sockfd, 
-                 (struct sockaddr *) &cli_addr, 
-                 &clilen_addr_size);
-         if (newsockfd < 0)
-             error("ERROR on accept");
-         pthread_create(thread, NULL, func, (void*)(&newsockfd));
-         
-     }
-
-     close(sockfd);
-     return 0; 
+    portno = atoi(argv[1]);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+    if (bind(sockfd, (struct sockaddr *) &serv_addr,
+             sizeof(serv_addr)) < 0)
+        error("ERROR on binding");
+    listen(sockfd,5);
+    clilen_addr_size = sizeof(cli_addr);
+    while (1)
+    {
+        newsockfd = accept(sockfd,
+                           (struct sockaddr *) &cli_addr,
+                           &clilen_addr_size);
+        if (newsockfd < 0)
+            error("ERROR on accept");
+        pthread_create(thread, NULL, func, (void*)(&newsockfd));
+        
+    }
+    
+    close(sockfd);
+    return 0;
 }
